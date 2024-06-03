96d9e4a0cc7b8bbb53c209c13c7d44f790f96d85
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-06-03 22:44:31 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
+"""for a given employee ID,
+returns information about his/her TODO list progress."""
 
+import requests
+import sys
+
+todo_url = "https://jsonplaceholder.typicode.com"
+
+if __name__ == '__main__':
+   if len(sys.argv) > 1:
+      if re.fullmatch(r'\d+', sys.argv[1]):
+         employee = request.get('name')
+         emp_id = int(sys.argv[1])
+         req = requests.get('{}/users/{}'.format(todo_url, emp_id)).json()
+         task_req = requests.get('{}/todos'.format(todo_url)).json()
+         tasks = list(filter(lambda x: x.get('emp_id') == emp_id, task_request))
+         task_done = list(filter(lambda x: x.get('completed'), tasks))
+
+         print(
+              'Employee {} is done with tasks({}/{}):'.format(
+				employee,len(task_done),len(tasks))
+	)
+      if len(task_done) > 0:
+         for task in task_done:
+             print('\t {}'.format(task.get('title')))

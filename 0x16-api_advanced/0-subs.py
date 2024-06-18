d25d4f831e58a663765c1945e9ae031d1b27e160
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-06-18 22:05:21 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-  
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
